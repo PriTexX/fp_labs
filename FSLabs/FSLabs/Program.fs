16dd25ec4fe7be52b92b857b89e2d728460b8551
pritexx <vladon_2004@mail.ru>
2026-02-21 13:37:40 +0300

    5 lab

---
+++ FSLabs/FSLabs/Program.fs
@@ -0,0 +1,17 @@
+﻿// Разработайте набор чистых функций для выполнения общих математических операций
+
+let add a b = a + b
+
+let subtract a b = a - b
+
+let multiply a b = a * b
+
+let divide a b = a / b
+
+// Напишите рекурсивную функцию для вычисления факториала числа.
+let rec fact n =
+    match n with
+    | n when n < 0 -> invalidArg "n" "Cant be negative"
+    | 0 | 1 -> 1
+    | n -> n * fact (n - 1)
+ 
